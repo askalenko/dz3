28add56fb76f432050a4c2248304dbf2b281334f
sashaom <sanderoffsky@gmail.com>
2019-03-10 10:18:13 +0200

    fixed points count
    Чтобы переменная result в цикле видела значение из переменной result функции (область видимости переменной), нужно ЯВНО возвращать  true или false в зависимости от ответа. Чтобы result в цикле смог отреагировать и добавить 10 очков.

--- dz3/app3.js
+++ dz3/app3.js
@@ -22,7 +22,7 @@ type: 'prompt'
 for (let i = 0; i < arr.length; i++) {
     let result =  askQuestion(arr[i]);
     points += result ? 10 : 0;  										
-    console.log('CHECK' + result)										//result не видит значений из функции
+    console.log('CHECK' + result + 'POINTS' + points)					//result не видит значений из функции
     // while (result == true) {											//как реализовать суммирование очков
     // 	points += 10;													//сюда цикл не заходит
     // 	console.log ('ALL pointSSS' + points)
@@ -36,18 +36,20 @@ function askQuestion(ask) {
 		if (ask.type == 'prompt') {				
 				let userAnswer = prompt(ask.question);
 				if (userAnswer == ask.answer ) {     					//валидация ?
-					result = true; 									
+					return result = true;								
 					// points =+ 10;					
 					 console.log('promt ' + points + ask);
 				} 
 		}  else if (ask.type == 'confirm') {
 			let userAnswer = confirm(ask.question);
 				if (userAnswer == true ) { 
-					result = true;
+					// result = true;
+					return result = true;
 						// points =+ 10;					
 						 console.log('confirm ' + points + ask);
 					}
 		} else {
 			return alert('not work')
 		}
+	return result = false;
 }
