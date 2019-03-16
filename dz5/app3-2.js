abd61d581825dc55d780ecf4cb8d6caa560a9683
sashaom <sanderoffsky@gmail.com>
2019-03-16 15:04:55 +0200

    Added TASK 2
    quantityEvenNumber не видет.
    
    Задание 2
    генерируем два рандомных целых числа от 1000 до 2000 и выводим большее из них.

--- dz5/app3-2.js
+++ dz5/app3-2.js
@@ -1,13 +1,15 @@
 
 
+// TASK 1 
 
+alert("ЗАДАЧА 1");
 
 
 function getNumber(validNumber) {
 	let someNumber = prompt('Введите число');   
 	if (!isNaN(someNumber))  {                               
 		return isNumberEven(someNumber);
-		// return quantityEvenNumber;
+		return quantityEvenNumber;
 	} else {
 		alert ('Вы ввели не число')
 		 getNumber();										
@@ -18,8 +20,6 @@ function getNumber(validNumber) {
 
 
 function isNumberEven(numString) {
-	// let comma = ',';
-	// console.log(comma);
 	let arrNumString = numString.split('');
 	console.log(arrNumString);
 	 for (var i = 0; i <= arrNumString[i]; i++) {
@@ -44,3 +44,39 @@ let result = getNumber();
 alert('В введенном числе ' + quantityEvenNumber + 'четных чисел');
 
 
+
+
+// __________________________________________________________________
+
+// _________________________TASK 2__________________________________
+
+// __________________________________________________________________
+
+alert("ЗАДАЧА 2");
+
+const minIntervalNumber = 1000;
+const maxIntervalNumber = 2000;
+
+
+
+function randomNumber(min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1)   //разобраться в логике работы 
+    rand = Math.round(rand);
+    return rand;
+  }
+
+
+function largeNumber(firstNumber, secondNumber) {
+	let firstRandomNumber = randomNumber(minIntervalNumber, maxIntervalNumber );
+	let secondRandomNumber = randomNumber(minIntervalNumber, maxIntervalNumber);
+	let maxNum = Math.max(firstRandomNumber, secondRandomNumber);
+	return maxNum;
+}
+
+let result2 = largeNumber();
+
+alert('наибольшее число из друх рандомных из 1000 и 2000 : ' + result2);
+
+
+
+// alert(maxNum);
